40650626d067ee49b490d36185035a00766c4f0b
Jorge Matos <jisapjisap@gmail.com>
2021-09-21 17:10:38 +0200

    SPA control

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const express = require('express');
 require('dotenv').config();
 var cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 
 const  app = express();
 
@@ -28,6 +29,10 @@ app.use( '/api/medicos', require('./routes/medicos'));
 app.use( '/api/todo', require('./routes/busquedas'));
 app.use( '/api/upload', require('./routes/uploads'));
 
+//Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ));
+});
 
 
 app.listen( process.env.PORT, () => {
